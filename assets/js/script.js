c0860a56f65027931948a0883e7fabdce449c804
pabco84 <pabco84@gmail.com>
2022-06-11 15:25:56 -0400

    primer

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let cantidad = document.querySelector('#cantidad');
+const cost = 1999000; 
+let b = document.querySelector('#boton');
+
+
+b.addEventListener("click", function(){
+totalcost = Number(cantidad.value) * cost;
+costototal.textContent = totalcost;
+cantidadset.textContent = cantidad.value;
+
+
+    let color = document.querySelector('#color').value;
+    colorset.style.BackgroundColor = color ;
+
+});
